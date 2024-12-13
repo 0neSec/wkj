fa7230146cf9d01fa3489816aaecc09f0101d517
Khadik86 <mohamadkhadik7@gmail.com>
2024-12-13 07:02:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
           <Route element={<LoginPage/>} path='/login'/>
           
           <Route element={<RegisterPage/>} path='/register'/>
+          <SpeedInsights/>
         </Routes>
       </Router>
     </>
